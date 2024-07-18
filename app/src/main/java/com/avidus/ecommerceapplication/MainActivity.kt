0146a4087a96c0881a2012ea5ddb6a1448eb4e00
anshikamishra1 <anshika@avidusinteractive.com>
2024-07-18 18:56:27 +0530

    Data Fetching Using RetroFit

--- app/src/main/java/com/avidus/ecommerceapplication/MainActivity.kt
+++ app/src/main/java/com/avidus/ecommerceapplication/MainActivity.kt
@@ -5,16 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.avidus.ecommerceapplication.data.MyDataClass
+import com.avidus.ecommerceapplication.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val retrofitBuilder= Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+
+        val retrofitData = retrofitBuilder.getProducts()
+        retrofitData.enqueue(object : Callback<MyDataClass?> {
+            override fun onResponse(
+                call: Call<MyDataClass?>,
+                response: Response<MyDataClass?>
+            ) {
+
+                val responseBody = response.body()!!
+                val productList = responseBody.products!!
+                binding.textView2.text=productList.toString();
+
+    }
+            override fun onFailure(call: Call<MyDataClass?>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
